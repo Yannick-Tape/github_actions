9aa61649d6f5815a97abd85a4ecea6dc14c1fe44
Yannick <tape.yannick@yahoo.fr>
2023-08-25 21:58:09 +0000

    respect de la convention PEP8

--- .github/workflows/file_test.py
+++ .github/workflows/file_test.py
@@ -1,17 +1,22 @@
 import pytest
 
 def test_calc_addition():
+    # Fonction de test du résultat de 2 + 4
     output = 2 + 4
     assert output == 6
 
-def test_calc_substraction():
+def test_calc_subtraction():
+    # Fonction de test du résultat de 2 - 4
     output = 2 - 4
     assert output == -2
 
 def test_calc_multiply():
+    # Fonction de test du résultat de 2 * 4
     output = 2 * 4
     assert output == 8
 
 def test_coucou():
-    output = "hello"
-    assert output == "hello"
+    # Fonction de test si le résultat renvoie 'hello'
+    output = 'hello'
+    assert output == 'hello'
+
